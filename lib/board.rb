0ebd9a5ecf6003143ce1404168ce452ced81657f
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-26 22:47:02 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -5,6 +5,13 @@ class Board
     reset!
   end
 
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
 
   def reset!
     @cells = Array.new(9,' ')
